804b48c455274c38e2b86746e5b2f024f2ba14ea
Kevin Armengol <kevin.armengol@publicissapient.com>
2023-09-22 10:55:30 -0400

    More troubleshooting streamlit app path.

--- studysearch_app.py
+++ studysearch_app.py
@@ -1,8 +1,9 @@
 import os
-import sys
-from pathlib import Path
 
-sys.path.append(str(Path(__file__).resolve().parent.parent))
+# import sys
+# from pathlib import Path
+
+# sys.path.append(str(Path(__file__).resolve().parent.parent))
 import streamlit as st
 import pandas as pd
 from dotenv import load_dotenv
